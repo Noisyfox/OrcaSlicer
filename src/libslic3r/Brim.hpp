df88718d3555f7b380eb121b91c83e28971000ab
Noisyfox <timemanager.rick@gmail.com>
2023-10-11 21:22:34 +0800

    Remove unused brim code

--- src/libslic3r/Brim.hpp
+++ src/libslic3r/Brim.hpp
@@ -15,7 +15,7 @@ class ObjectID;
 
 // Produce brim lines around those objects, that have the brim enabled.
 // Collect islands_area to be merged into the final 1st layer convex hull.
-ExtrusionEntityCollection make_brim(const Print& print, PrintTryCancel try_cancel, Polygons& islands_area);
+//ExtrusionEntityCollection make_brim(const Print& print, PrintTryCancel try_cancel, Polygons& islands_area);
 void make_brim(const Print& print, PrintTryCancel try_cancel,
     Polygons& islands_area, std::map<ObjectID, ExtrusionEntityCollection>& brimMap,
     std::map<ObjectID, ExtrusionEntityCollection>& supportBrimMap,
@@ -23,7 +23,7 @@ void make_brim(const Print& print, PrintTryCancel try_cancel,
     std::vector<unsigned int>& printExtruders);
 
 // BBS: automatically make brim
-ExtrusionEntityCollection make_brim_auto(const Print &print, PrintTryCancel try_cancel, Polygons &islands_area);
+//ExtrusionEntityCollection make_brim_auto(const Print &print, PrintTryCancel try_cancel, Polygons &islands_area);
 
 } // Slic3r
 
