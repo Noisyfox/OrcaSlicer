ee9ef7f877fc520b3da587688ee504bf86aa4db1
zhou.xu <zhou.xu@bambulab.com>
2024-10-16 10:49:49 +0800

    NEW:add "extruder_only_area" textures for
    double extruder machine
    jira:none
    
    Change-Id: Iaf4118fb00641537838dae7cc5fdaaf07ccc9851
    (cherry picked from commit a8d6c9fe8976dbcb79c1f89efb7107073cb17856)
    (cherry picked from commit c74a769c14d702dbb78d479e49be927028502037)

--- src/libslic3r/PrintConfig.hpp
+++ src/libslic3r/PrintConfig.hpp
@@ -289,6 +289,13 @@ enum BedType {
     btCount
 };
 
+enum class ExtruderOnlyAreaType:unsigned char {
+    btNoArea= 0,
+    Engilish,
+    Chinese,
+    btAreaCount
+};
+
 // BBS
 enum LayerSeq {
     flsAuto, 
