e7b3b8767d218b036a9316ba23f31cb9fa181e04
Noisyfox <timemanager.rick@gmail.com>
2026-07-23 22:49:27 +0800

    Don't build wxInspector sample

--- deps/wxInspector/wxInspector.cmake
+++ deps/wxInspector/wxInspector.cmake
@@ -1,7 +1,7 @@
 orcaslicer_add_cmake_project(
     wxInspector
     GIT_REPOSITORY https://github.com/Noisyfox/wxInspector.git
-    GIT_TAG        e4428bf5fadc505e79b2142060652d95413736a2 
+    GIT_TAG        e27ed8eca4e68cd1e64e20328a9706d97668eb65 
     DEPENDS dep_wxWidgets
     CMAKE_ARGS
         -DCMAKE_CXX_FLAGS="-DwxDEBUG_LEVEL=0"
