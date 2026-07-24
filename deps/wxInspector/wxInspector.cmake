a673c4052a171eb434980444f27cbdb549634ddf
Noisyfox <timemanager.rick@gmail.com>
2026-07-24 09:27:18 +0800

    Use shallow clone

--- deps/wxInspector/wxInspector.cmake
+++ deps/wxInspector/wxInspector.cmake
@@ -2,6 +2,7 @@ orcaslicer_add_cmake_project(
     wxInspector
     GIT_REPOSITORY https://github.com/Noisyfox/wxInspector.git
     GIT_TAG        e27ed8eca4e68cd1e64e20328a9706d97668eb65 
+    GIT_SHALLOW ON
     DEPENDS dep_wxWidgets
     CMAKE_ARGS
         -DCMAKE_CXX_FLAGS="-DwxDEBUG_LEVEL=0"
